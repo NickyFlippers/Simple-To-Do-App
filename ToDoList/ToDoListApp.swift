b96fdfdf08b12de01b42828071fdd68576124526
NickyFlippers <118223056+NickyFlippers@users.noreply.github.com>
2023-03-23 19:08:58 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Nicholas Gramenos on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
